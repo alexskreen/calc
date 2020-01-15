bd28ea134966f1ee4ce01e19f0edc8c76ab1b377
Student <example@example.com>
2020-01-15 10:07:33 -0800

    make calc funciton using form, and have multipul buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,51 @@
+$(document).ready(function(){
 var add = function(number1, number2) {
+    console.log("test");
     return number1 + number2;
-    };
     
-    var number1 = parseInt(prompt("Enter a number:"));
-    var number2 = parseInt(prompt("Enter another number:"));
-    
-    
-    alert (add(number1,number2));
+};
 
-
-    
-    
-var subtract = function(number1, number2) {
+  var subtract = function(number1, number2) {
     return number1 - number2;
-    };
-    
-    var number1 = parseInt(prompt("Enter a number:"));
-    var number2 = parseInt(prompt("Enter another number:"));
-    
-    
-    alert (subtract(number1,number2));
-
-
-var multiply = function(number1, number2) {
+  };
+  
+  var multiply = function(number1, number2) {
     return number1 * number2;
-    };
-    
-    var number1 = parseInt(prompt("Enter a number:"));
-    var number2 = parseInt(prompt("Enter another number:"));
-    
-    
-    alert (multiply(number1,number2));
-
-
-var divide = function(number1, number2) {
+  };
+  
+  var divide = function(number1, number2) {
     return number1 / number2;
-    };
-    
-    var number1 = parseInt(prompt("Enter a number:"));
-    var number2 = parseInt(prompt("Enter another number:"));
-    
-    
-    alert (divide(number1,number2));
+  };
+    $("#add-btn").click(function(){
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    $(".answer").text(add(number1, number2));
+    $(".result").show();
+    event.preventDefault();
+    });
+    $("#sub-btn").click(function(){
+        var number1 = parseInt($("#add1").val());
+        var number2 = parseInt($("#add2").val());
+        $(".answer").text(subtract(number1, number2));
+        $(".result").show();
+        event.preventDefault();
+        });
+        $("#mult-btn").click(function(){
+        var number1 = parseInt($("#add1").val());
+        var number2 = parseInt($("#add2").val());
+        $(".answer").text(multiply(number1, number2));
+        $(".result").show();
+        event.preventDefault();
+        });
+        $("#div-btn").click(function(){
+            var number1 = parseInt($("#add1").val());
+            var number2 = parseInt($("#add2").val());
+            $(".answer").text(divide(number1, number2));
+            $(".result").show();
+            event.preventDefault();
+            });
+
+
+
+ 
+});
